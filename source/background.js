86313453cabecfa37adccc961cb6fe7bc79c48c9
gamemastr <bladesimmer@gmail.com>
2015-01-13 20:19:43 -0800

    added comments

--- source/background.js
+++ source/background.js
@@ -1,11 +1,13 @@
 var host = "http://i.imgur.com";
 chrome.webRequest.onBeforeRequest.addListener(
     function(details) {
-         return {redirectUrl: host + details.url.match(/^https?:\/\/[^\/]+([\S\s]*)/)[1] + "v"};
+         return {redirectUrl: host + details.url.match(/^https?:\/\/[^\/]+([\S\s]*)/)[1] + "v"}; //i... i don't even know...
+         																						 //the + "v" changes gif > gifv, so it works
     },
     {
         urls: [
-            "http://i.imgur.com/*.gif"
+            "http://i.imgur.com/*.gif" //only activate on .gif page
+            						   //causes redirect loop if removed!
         ],
     },
     ["blocking"]
