b6eae59a63bcca003e896ddb06373951572cd712
Gabriel Simmer <bladesimmer@gmail.com>
2015-01-13 20:08:44 -0800

    Create background.js

---
+++ source/background.js
@@ -0,0 +1,12 @@
+var host = "http://i.imgur.com";
+chrome.webRequest.onBeforeRequest.addListener(
+    function(details) {
+         return {redirectUrl: host + details.url.match(/^https?:\/\/[^\/]+([\S\s]*)/)[1] + "v"};
+    },
+    {
+        urls: [
+            "http://i.imgur.com/*.gif"
+        ],
+    },
+    ["blocking"]
+);
